6825762861beaf19e713034b569b5b01d431c1a3
comiser <comiser@comiser.comiser>
2024-10-15 04:03:06 +0330

    delete bugs

--- _api/product/fetchProducts.js
+++ _api/product/fetchProducts.js
@@ -1,16 +1,15 @@
 export const fetchProducts = async () => {
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
-    const res = await fetch(`http://127.0.0.1:8000/api/product/products/`, {
-        method: 'GET',
-        cache: 'force-cache',  // کش کردن داده‌ها تا زمانی که نیاز به رفرش باشد
-        next: { revalidate: 10 },  // داده‌ها هر 60 ثانیه یکبار رفرش می‌شوند
-    });
+  // await new Promise((resolve) => setTimeout(resolve, 3000));
+  const res = await fetch(`http://127.0.0.1:8000/api/product/products/`, {
+    method: "GET",
+    next: { revalidate: 10 }, // داده‌ها هر 60 ثانیه یکبار رفرش می‌شوند
+  });
 
-    if (!res.ok) {
-        const error = await res.json();
-        throw new Error(error.detail || 'خطا در دریافت اطلاعات');
-    }
+  if (!res.ok) {
+    const error = await res.json();
+    throw new Error(error.detail || "خطا در دریافت اطلاعات");
+  }
 
-    const data = await res.json();
-    return data;
+  const data = await res.json();
+  return data;
 };
--- middleware.js
+++ middleware.js
@@ -1,46 +1,41 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 import { verifyToken } from "@/app/utils/utils";
 import { refreshAccessToken } from "@/app/utils/refreshAccessToken";
 
 export async function middleware(request) {
-    const response = NextResponse.next();
-    const accessToken = request.cookies.get('access_token')?.value;
-    const refreshToken = request.cookies.get('refresh_token')?.value;
+  const response = NextResponse.next();
+  const accessToken = request.cookies.get("access_token")?.value;
+  const refreshToken = request.cookies.get("refresh_token")?.value;
 
-    if (accessToken) {
-        const { valid, error } = await verifyToken(accessToken);
+  if (accessToken) {
+    const { valid, error } = verifyToken(accessToken);
 
-        if (!valid) {
-            console.warn('Access token is invalid. Trying to refresh...');
+    if (!valid) {
+      if (refreshToken) {
+        try {
+          const newToken = await refreshAccessToken(refreshToken);
 
-
-            if (refreshToken) {
-                try {
-                    const newToken = await refreshAccessToken(refreshToken);
-
-
-                    if (newToken) {
-                        const response = NextResponse.next();
-                        response.cookies.set('access_token', newToken, { httpOnly: true, path: '/',  maxAge: 60 * 60, });
-                        return response;
-                    }
-                } catch (err) {
-                    console.error('Error refreshing access token:', err);
-                }
-            }
-        } else {
-
-            return NextResponse.next();
+          if (newToken) {
+            const response = NextResponse.next();
+            response.cookies.set("access_token", newToken, {
+              httpOnly: true,
+              path: "/",
+              maxAge: 60 * 60,
+            });
+            return response;
+          }
+        } catch (err) {
+          throw new Error("Error refreshing access token:", err);
         }
+      }
+    } else {
+      return NextResponse.next();
     }
+  }
 
-    // اگر هیچ توکنی وجود ندارد یا توکن جدید نتوانسته ایجاد شود
-    console.warn('No valid access token or refresh token found.');
-
-    // هدایت به صفحه خطا
-    return  response;
+  return response;
 }
 
 export const config = {
-    matcher: ['/((?!_next/static|favicon.ico).*)'], // تمام مسیرها به جز فایل‌های استاتیک
+  matcher: ["/((?!_next/static|favicon.ico).*)"],
 };
